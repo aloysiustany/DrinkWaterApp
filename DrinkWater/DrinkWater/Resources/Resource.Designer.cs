41d1f9ee27d9d92f6faed66470c9f50d0bbc8b56
Tany <home@HOME-PC>
2016-06-18 16:23:13 +0530

    18 june home

--- DrinkWater/DrinkWater/Resources/Resource.Designer.cs
+++ DrinkWater/DrinkWater/Resources/Resource.Designer.cs
@@ -63,16 +63,19 @@ namespace DrinkWater
 			public const int Icon = 2130837509;
 			
 			// aapt resource value: 0x7f020006
-			public const int Plus_Math_60_Pressed = 2130837510;
+			public const int MAHeaderSummaryBox = 2130837510;
 			
 			// aapt resource value: 0x7f020007
-			public const int Plus_Math_60_Shadow = 2130837511;
+			public const int Plus_Math_60_Pressed = 2130837511;
 			
 			// aapt resource value: 0x7f020008
-			public const int Recurring_Icon_60_Pressed = 2130837512;
+			public const int Plus_Math_60_Shadow = 2130837512;
 			
 			// aapt resource value: 0x7f020009
-			public const int Recurring_Icon_60_Shadow = 2130837513;
+			public const int Recurring_Icon_60_Pressed = 2130837513;
+			
+			// aapt resource value: 0x7f02000a
+			public const int Recurring_Icon_60_Shadow = 2130837514;
 			
 			static Drawable()
 			{
